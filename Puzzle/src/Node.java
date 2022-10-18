6f8bd93713669bf8f8e4f04de1a6b32598a18a35
Marwan-Mahmoud <108197024+Marwan-Mahmoud@users.noreply.github.com>
2022-10-18 21:10:48 +0200

    Rearrange Node.java

--- Puzzle/src/Node.java
+++ Puzzle/src/Node.java
@@ -1,157 +1,101 @@
-
-
-import java.util.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Node {
-    private int x;
-    private int y;
-    private Node parent;
-    private int[][] state = new int[3][3];
 
-    public void setNeighbours(List<Node> neighbours) {
-        this.neighbours = neighbours;
-    }
+	private int[][] state = new int[3][3];
+	private int x;
+	private int y;
+	private Node parent;
+	private List<Node> neighbours = new ArrayList<>();
+	private int cost;
+	private String path = "";
+	private int depth;
+	private int maxDepth = 0;
 
-    private List<Node> neighbours = new ArrayList<>();
-    private String path = "";
-    private int cost;
-    private int depth;
+	// right, left, down,up
+	private int[] row = { 0, 0, 1, -1 };
+	private int[] col = { 1, -1, 0, 0 };
 
-    public int getMaxDepth() {
-        return maxDepth;
-    }
-    private int maxDepth = 0;
+	Node(int x, int y, Node parent, int[][] state, int cost, String path) {
+		this.x = x;
+		this.y = y;
+		this.parent = parent;
+		this.state = state;
+		this.cost = cost;
+		this.path = path;
+		this.maxDepth = Math.max(this.maxDepth, this.cost);
+	}
 
-    Node(int x, int y,Node parent, int[][] state, int cost, String path) {
-        this.x = x;
-        this.y = y;
-        this.parent = parent;
-        this.state = state;
-        this.cost = cost;
-        this.path = path;
-        this.maxDepth = Math.max(this.maxDepth,this.cost);
+	private void getAllNeighbours(Node currNode) {
+		for (int i = 0; i < 4; i++) {
+			int[][] tempState = new int[3][3];
+			copyMatrix(state, tempState);
+			int tempX = x + row[i];
+			int tempY = y + col[i];
+			if (isSafe(tempX, tempY)) {
+				int temp = tempState[x][y];
+				tempState[x][y] = tempState[tempX][tempY];
+				tempState[tempX][tempY] = temp;
+				if (i == 0) {
+					Node tempNode = new Node(tempX, tempY, currNode, tempState, cost + 1, path + " R");
+					neighbours.add(tempNode);
+				} else if (i == 1) {
+					Node tempNode = new Node(tempX, tempY, currNode, tempState, cost + 1, path + " L");
+					neighbours.add(tempNode);
+				} else if (i == 2) {
+					Node tempNode = new Node(tempX, tempY, currNode, tempState, cost + 1, path + " D");
+					neighbours.add(tempNode);
+				} else {
+					Node tempNode = new Node(tempX, tempY, currNode, tempState, cost + 1, path + " U");
+					neighbours.add(tempNode);
+				}
+			}
+		}
+	}
 
-    }
+	private void copyMatrix(int[][] state, int[][] tempState) {
+		for (int k = 0; k < 3; k++) {
+			for (int l = 0; l < 3; l++) {
+				tempState[k][l] = state[k][l];
+			}
+		}
+	}
 
-    private void copyMatrix(int[][] state, int[][] tempState) {
-        for (int k = 0; k < 3; k++) {
-            for (int l = 0; l < 3; l++) {
-                tempState[k][l] = state[k][l];
-            }
-        }
-    }
+	private static boolean isSafe(int x, int y) {
+		return (x >= 0 && x < 3 && y >= 0 && y < 3);
+	}
 
+	public List<Node> getNeighbors(Node currNode) {
+		getAllNeighbours(currNode);
+		return neighbours;
+	}
 
-    // right, left, down,up
-    private int row[] = {0, 0, 1, -1};
-    private int col[] = {1, -1, 0, 0};
+	public void setNeighbours(List<Node> neighbours) {
+		this.neighbours = neighbours;
+	}
 
-    private static boolean isSafe(int x, int y) {
-        return (x >= 0 && x < 3 && y >= 0 && y < 3);
-    }
+	public int[][] getState() {
+		return state;
+	}
 
-    private void getAllNeighbours(Node currNode) {
-        for (int i = 0; i < 4; i++) {
-            int[][] tempState = new int[3][3];
-            copyMatrix(state, tempState);
-            int tempX = x + row[i];
-            int tempY = y + col[i];
-            if (isSafe(tempX, tempY)) {
-                int temp = tempState[x][y];
-                tempState[x][y] = tempState[tempX][tempY];
-                tempState[tempX][tempY] = temp;
-                if (i == 0) {
-                    Node tempNode = new Node(tempX, tempY,currNode,tempState, cost + 1, path + " R");
-                    neighbours.add(tempNode);
-                } else if (i == 1) {
-                    Node tempNode = new Node(tempX, tempY,currNode ,tempState, cost + 1, path + " L");
-                    neighbours.add(tempNode);
-                } else if (i == 2) {
+	public void setState(int[][] state) {
+		this.state = state;
+	}
 
-                    Node tempNode = new Node(tempX, tempY, currNode,tempState, cost + 1, path + " D");
-                    neighbours.add(tempNode);
-                } else {
-                    Node tempNode = new Node(tempX, tempY, currNode,tempState, cost + 1, path + " U");
-                    neighbours.add(tempNode);
-                }
+	public String getPath() {
+		return path;
+	}
 
-            }
-        }
+	public int getCost() {
+		return cost;
+	}
 
-    }
+	public int getMaxDepth() {
+		return maxDepth;
+	}
 
-
-    public List<Node> getNeighbors(Node currNode) {
-
-        getAllNeighbours(currNode);
-        /*goToUp();
-        goToLeft();
-        goToRight();
-        goToDown();*/
-        return neighbours;
-    }
-    public String getPath(){
-        return path;
-    }
-    public int getCost(){
-        return cost;
-    }
-    public void setState(int[][] state) {
-        this.state = state;
-    }
-    public int[][] getState() {
-        return state;
-    }
-    public Node getParent(){
-        return parent;
-    }
-
-
-
-
-//    private void goToRight(){
-//        int[][] tempState = new int[3][3];
-//        copyMatrix(state,tempState);
-//        if(y+1<3){
-//            int temp = tempState[x][y];
-//            tempState[x][y] = tempState[x][y+1];
-//            tempState[x][y+1] = temp;
-//            Node tempNode = new Node(x,y+1,tempState,cost+1,path+" R");
-//            neighbours.add(tempNode);
-//        }
-//    }
-//    private void goToLeft(){
-//        int[][] tempState = new int[3][3];
-//        copyMatrix(state,tempState);
-//        if(y-1>=0){
-//            int temp = tempState[x][y];
-//            tempState[x][y] = tempState[x][y-1];
-//            tempState[x][y-1] = temp;
-//            Node tempNode = new Node(x,y-1,tempState,cost+1,path+" L");
-//            neighbours.add(tempNode);
-//        }
-//    }
-//    private void goToUp(){
-//        int[][] tempState = new int[3][3];
-//        copyMatrix(state,tempState);
-//        if(x-1>=0){
-//            int temp = tempState[x][y];
-//            tempState[x][y] = tempState[x-1][y];
-//            tempState[x-1][y] = temp;
-//            Node tempNode = new Node(x-1,y,tempState,cost+1,path+" U");
-//            neighbours.add(tempNode);
-//        }
-//    }
-//    private void goToDown(){
-//        int[][] tempState = new int[3][3];
-//        copyMatrix(state,tempState);
-//        if(x+1<3){
-//            int temp = tempState[x][y];
-//            tempState[x][y] = tempState[x+1][y];
-//            tempState[x+1][y] = temp;
-//            Node tempNode = new Node(x+1,y,tempState,cost+1,path+" D");
-//            neighbours.add(tempNode);
-//        }
-//    }
-
+	public Node getParent() {
+		return parent;
+	}
 }
