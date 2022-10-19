e00e9e5caa472a37b9ba139f6ea73072f3b4a999
Marwan-Mahmoud <108197024+Marwan-Mahmoud@users.noreply.github.com>
2022-10-19 20:32:57 +0200

    Removed old methods calls

--- Puzzle/src/aStarEuclidean.java
+++ Puzzle/src/aStarEuclidean.java
@@ -1,16 +1,18 @@
-public class aStarEuclidean extends aStar{
-    public aStarEuclidean(int[][] board) {
-        super(board);
-    }
-    @Override
-    public int calcHeuristic(int[][] board) {
-        int heuristic = 0;
-        for(int i = 0; i < 3; i++){
-            for(int j = 0; j < 3; j++) {
-                int correctY = board[i][j] / 3, correctX = board[i][j] % 3;
-                heuristic += Math.sqrt( (j - correctX) * (j - correctX) + (i - correctY) * (i - correctY));
-            }
-        }
-        return heuristic;
-    }
+public class aStarEuclidean extends aStar {
+
+	public aStarEuclidean(int[][] board) {
+		super(board);
+	}
+
+	@Override
+	public int calcHeuristic(int[][] board) {
+		int heuristic = 0;
+		for (int i = 0; i < 3; i++) {
+			for (int j = 0; j < 3; j++) {
+				int correctY = board[i][j] / 3, correctX = board[i][j] % 3;
+				heuristic += Math.sqrt((j - correctX) * (j - correctX) + (i - correctY) * (i - correctY));
+			}
+		}
+		return heuristic;
+	}
 }
